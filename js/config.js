4078e53c9098b257af0130ec2b5bfac3f647f633
abhidnyaraut11 <abhidnyaabhay.raut@utdallas.edu>
2025-11-18 22:38:14 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_zxsl0QPS1', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3bbjacnsbe4po7r8n1cq6cg4ks', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '3gt794peaen8376k2e12ekv60g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
